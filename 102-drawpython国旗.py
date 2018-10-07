720a1762a417aa6dac4f6190b2112e82d73ae95c
117010800102 <43240318+117010800102@users.noreply.github.com>
2018-10-07 13:09:45 +0800

    Add files via upload

---
+++ 102-drawpython国旗.py
@@ -0,0 +1,59 @@
+import turtle 
+turtle.bgcolor("red")
+turtle.fillcolor("yellow")
+turtle.color('yellow')
+turtle.speed(1)
+
+#大星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-600,220) 
+turtle.down()
+for i in range (5):    
+    turtle.forward(150)
+    turtle.right(144)
+turtle.end_fill()
+
+#小星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,295)
+turtle.setheading(305)
+turtle.down()
+for i in range (5):    
+    turtle.forward(50)
+    turtle.left(144)
+turtle.end_fill()
+
+#小星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,212)
+turtle.setheading(30)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.right(144)
+turtle.end_fill()
+
+#小星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,145)
+turtle.setheading(5)
+turtle.down()
+for i in range (5):   
+    turtle.forward(50)
+    turtle.right(144)
+turtle.end_fill()
+
+#小星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,90)
+turtle.setheading(300)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.left(144)
+turtle.end_fill()
