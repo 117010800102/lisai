274462a7d317591bc1da0bac9c8906b4f5537ce2
117010800102 <43240318+117010800102@users.noreply.github.com>
2018-10-12 09:57:14 +0800

    Add files via upload

---
+++ ex2-7.py
@@ -0,0 +1,13 @@
+import turtle as t
+t.setup(650,350,200,200)
+t.pensize(5)
+t.pencolor("red")
+for i in (270,210,150,90,30,-30):
+    t.seth(i)
+    t.fd(60)
+    t.seth(i-120)
+    t.fd(60)
+    t.seth(i-240)
+    t.fd(120)
+t.down
+t.seth(30)
