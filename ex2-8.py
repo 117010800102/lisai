5d9fdfdc17aa087064da0b82a03c80a6186b0923
117010800102 <43240318+117010800102@users.noreply.github.com>
2018-10-14 15:19:14 +0800

    Add files via upload

---
+++ ex2-8.py
@@ -0,0 +1,21 @@
+from turtle import*
+pensize(1)
+pencolor("red")
+i=1
+while(i<=160):
+    seth(90)
+    fd(i)
+    seth(180)
+    fd(i+1)
+    seth(-90)
+    fd(i+2)
+    seth(0)
+    fd(i+3)
+    i=i+4
+seth(90)
+fd(161)
+seth(180)
+fd(162)
+seth(-90)
+fd(163)
+seth(0)
